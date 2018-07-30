074f329b3f61a74a036b3ab2a2885a2132b76a54
Jonathan T. Miles <jonathan.t.miles@gmail.com>
2018-07-30 14:20:10 -0700

    implemented AskQUestion VC and outlets

---
+++ Q-and-A/Q-and-A/ViewControllers/AskQuestionViewController.swift
@@ -0,0 +1,39 @@
+//
+//  AskQuestionViewController.swift
+//  Q-and-A
+//
+//  Created by Jonathan T. Miles on 7/30/18.
+//  Copyright © 2018 Jonathan T. Miles. All rights reserved.
+//
+
+import UIKit
+
+class AskQuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func submitQuestion(_ sender: Any) {
+    }
+    @IBOutlet weak var askerTextField: UITextField!
+    
+    @IBOutlet weak var questionTextView: UITextView!
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- Q-and-A/Q-and-A/ViewControllers/QuestionTableViewCell.swift
+++ Q-and-A/Q-and-A/ViewControllers/QuestionTableViewCell.swift
@@ -20,5 +20,8 @@ class QuestionTableViewCell: UITableViewCell {
 
         // Configure the view for the selected state
     }
-
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var askedByLabel: UILabel!
+    @IBOutlet weak var thirdLabel: UILabel!
+    
 }
